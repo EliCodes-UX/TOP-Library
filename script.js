2f9ecad0bb1830c8501a7866a3755498f16bc7c7
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-01-23 12:08:22 -0700

    added more Javascript

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+const myLibrary = [];
 const bookModal = document.querySelector('.bookModal');
-const addBookModal = document.querySelector('.openModal');
-const cancelBookModal = document.querySelector('.cancelBook');
+const addBookModalBtn = document.querySelector('.openModal');
+const cancelBookModalBtn = document.querySelector('.cancelBook');
+const submitNewBookBtn = document.querySelector('submitNewBook');
+const authorInput = document.querySelector('author');
+const bookTitleInput = document.querySelector('bookTitle');
+const pagesInput = document.querySelector('pages');
+const readInput = document.querySelector('ifRead');
+const bookList = document.querySelector('bookTable');
 
-addBookModal.addEventListener('click', () => {
+function book(name, author, pages, read) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+console.log(book);
+addBookModalBtn.addEventListener('click', () => {
   bookModal.showModal();
 });
-cancelBookModal.addEventListener('click', () => {
+cancelBookModalBtn.addEventListener('click', () => {
   bookModal.close();
 });
+
+const addBookToLibrary = (name, author, pages, read) => {
+  myLibrary.push(new book(name, author, pages, read));
+};
